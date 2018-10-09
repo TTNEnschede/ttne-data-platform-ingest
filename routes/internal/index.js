6c443e980569e9eb6fb636abb6c1a3bb7b31a3ce
Timothy Sealy <timothy.sealy@gmail.com>
2018-10-10 00:56:10 +0200

    Added kill path for the chaos monkey test to use.

--- routes/internal/index.js
+++ routes/internal/index.js
@@ -25,4 +25,27 @@ router.get('/healthy', function (req, res, next) {
   next()
 })
 
+/**
+ * @api {get} /internal/kill Kill the service
+ * @apiVersion 1.0.0
+ * @apiHeader {String} apikey An access-key for health checking.
+ * @apiDescription With this api end point the service can be killed.
+ *
+ * @apiGroup Internal
+ * @apiSuccess {String} result The result message.
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    {
+ *      result: "dying"
+ *    }
+ * @apiErrorExample {json} Errors
+ *    HTTP/1.1 500 Internal Server Error
+ *    HTTP/1.1 400 Bad Request
+ */
+router.get('/kill', function (req, res, next) {
+  res.send({ result: 'dying' })
+  next()
+  process.exit(0)
+})
+
 module.exports = router;
